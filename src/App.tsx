d7a4b3a9f971883156bee571bef9b09bbb50bab2
Jin Won Lee <jinwonlee0916@gmail.com>
2025-08-10 02:07:36 -0400

    Remove ProjectDetails file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ProjectDetails from "./pages/ProjectDetails";
 import Projects from "./pages/Projects";
 import Blog from "./pages/Blog";
 import BlogPost from "./features/BlogPosts";
@@ -12,7 +11,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/:projectName" element={<ProjectDetails />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
         </Routes>
